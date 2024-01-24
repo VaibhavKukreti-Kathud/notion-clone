9e1b57269949958cee76ed283f4a1667d7351537
Vaibhav Kukreti <9720440160beena@gmail.com>
2024-01-24 11:16:55 +0530

    Add SettingsModal component to display user settings

--- components/modals/settings-modal.tsx
+++ components/modals/settings-modal.tsx
@@ -4,3 +4,20 @@ import { Dialog, DialogContent, DialogHeader } from "@/components/ui/dialog";
 import { useSettings } from "@/hooks/use-settings";
 import { Label } from "@/components/ui/label";
 import { ModeToggle } from "@/components/theme-mode-toggle";
+
+export const SettingsModal = () => {
+  const settings = useSettings();
+
+  return (
+    <Dialog open={settings.isOpen} onOpenChange={settings.onClose}>
+      <DialogContent>
+        <DialogHeader className="border-b pb-3">
+          <h2 className="text-lg font-medium">My Settings</h2>
+        </DialogHeader>
+        <div className="flex items-center justify-between">
+          <Label>Theme</Label>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
